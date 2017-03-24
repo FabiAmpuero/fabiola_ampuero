3c4ed4433e88c6e7cba4de5f199cadd34b461120
Fabiola <fabi.ampuero.chalco@gmail.com>
2017-03-24 08:48:14 -0500

    se agrego secciones y se modifico menu

--- js/header.js
+++ js/header.js
@@ -1,3 +1,34 @@
 function init() {
-    alert("oli");
+    
+    anime = setInterval(animacion,1000);
+    
+    var heightWindow = $(window).height();
+    $("header").css("height",heightWindow);
+    var alto = heightWindow-100;
+    $(".hero").css("padding-top",alto/2);
 }
+
+function animacion() {
+    $("#let_h").animate({display: 'block'});
+    $("#let_o").animate({bottom: '16px'});
+    $("#let_l").animate({display: 'block'});
+    $("#let_a").animate({bottom: '16px'});
+    clearInterval(anime);
+    clear = setInterval(desaparecer,3000);
+}
+
+function desaparecer() {
+    console.log("oli");
+    $("#let_h").fadeOut();
+    $("#let_o").fadeOut();
+    $("#let_l").fadeOut();
+    $("#let_a").fadeOut();
+    clearInterval(clear);
+    nombre = setInterval(nombreIn,1000);
+}
+
+function nombreIn() {
+    $("#nombre").fadeIn();
+}
+
+
