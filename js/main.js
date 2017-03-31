ee986b1d65f5fc39d7b6b6469e7e9182f0d85ce0
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2017-03-31 13:19:33 -0500

    se cambio background

--- js/main.js
+++ js/main.js
@@ -5,9 +5,18 @@ $(document).ready(function(){
 		if ( $(window).scrollTop() > 0 ){
 			$('.menu').addClass('menu-fijo');
             $('#logo-img').attr("src","img/logo_purpple.png");
+            $(".a-about").html("SOBRE MÍ");
+            $(".a-skill").html("HABILIDADES");
+            $(".a-portfolio").html("PORTAFOLIO");
+            $(".a-contact").html("CONTACTO");
 		} else {
 			$('.menu').removeClass('menu-fijo');
             $('#logo-img').attr("src","img/logo_white.png");
+            
+            $(".a-about").html("");
+            $(".a-skill").html("");
+            $(".a-portfolio").html("");
+            $(".a-contact").html("");
 		}
 	});
  
